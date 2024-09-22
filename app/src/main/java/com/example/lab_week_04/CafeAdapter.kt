1eeb65a3f36923ce642bb699c231d8fbcba0ebbd
sharonerosee <129519148+sharonerosee@users.noreply.github.com>
2024-09-23 01:38:14 +0700

    update code
    clear unnecessary code

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -7,12 +7,6 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-//val TABS_FIXED = listOf(
-//    R.string.starbucks_title,
-//    R.string.janjijiwa_title,
-//    R.string.kopikenangan_title,
-//)
-
 class CafeAdapter(
     private val context: Context,
     fragmentManager: FragmentManager,
@@ -23,18 +17,11 @@ class CafeAdapter(
         Pair(R.string.janjijiwa_title, R.string.janjijiwa_desc),
         Pair(R.string.kopikenangan_title, R.string.kopikenangan_desc)
     )
-//        private val tabFragment = listOf(
-//            CafeDetailFragment.newInstance("Starbucks"),
-//            CafeDetailFragment.newInstance("Janji Jiwa"),
-//            CafeDetailFragment.newInstance("Kopi Kenangan"),
-//            )
 
     override fun getItemCount(): Int {
-//        return tabFragment.size
           return cafeData.size
     }
     override fun createFragment(position: Int): Fragment {
-//        return CafeDetailFragment()
         val (titleResId, descriptionResId) = cafeData[position]
         val title = context.getString(titleResId)
         val description = context.getString(descriptionResId)
