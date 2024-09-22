42fb1cd12b3d7ab83dc95d85193d708575c7f73e
sharonerosee <129519148+sharonerosee@users.noreply.github.com>
2024-09-23 00:55:25 +0700

    fix. CafeDetailFragment class
    fix error code cause wrong position of code

--- app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
@@ -7,24 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val TAB_CONTENT = "TAB_CONTENT"
+
 
 class CafeDetailFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-    private const val TAB_CONTENT = "TAB_CONTENT"
-    class CafeDetailFragment : Fragment(){
         private var content: String? = null
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             content = it.getString(TAB_CONTENT)
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -41,14 +33,11 @@ class CafeDetailFragment : Fragment() {
             ?.text = content
     }
 
-
     companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(content: String) =
             CafeDetailFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(TAB_CONTENT, content)
                 }
             }
     }
